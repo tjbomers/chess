3a125c20d950587bc6f8a5a67f650dc795f3ce1e
bomerst <bomerst@mail.gvsu.edu>
2019-03-27 00:08:06 -0400

    Merge branch 'master' of C:\Users\tjbomers\Desktop\School\2019\Winter\CIS 163\Projects\Project 3\chess with conflicts.

--- Rook.java
+++ Rook.java
@@ -31,24 +31,6 @@ public class Rook extends ChessPiece {
 	 */
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
 
-		//Checks to see if the Rook is trying to move on a diagonal, which is
-		//illegal for that piece.
-		if (move.fromRow != move.toRow && move.fromColumn != move.toColumn) {
-			return false;
-		}
-
-		//This will check to see if the Rook is moving left, right, up, or down.  This
-		//is accomplished by checking to see if it moves 0 spaces horizontally OR
-		//0 spaces vertically
-		if (board[move.fromRow][move.fromColumn] != board[move.toRow][move.toColumn]) {
-			if ((move.fromRow - move.toRow == 0) || (move.fromColumn - move.toColumn == 0)) {
-				if (board[move.toRow][move.toColumn] != null)
-					if (board[move.toRow][move.toColumn].player() == (board[move.fromRow][move.fromColumn].player())) {
-						return false;
-					}
-				return true;
-			}
-		}
-		return false;
+		return true;
 	}
 }
