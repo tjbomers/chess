3618273eba0e55ffa4ce6a29aeb865cc83ebef71
olivemag <48969762+olivemag@users.noreply.github.com>
2019-03-28 22:33:40 -0400

    Add files via upload

---
+++ ChessModelTest.java
@@ -0,0 +1,76 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChessTest {
+
+
+
+    @Test
+    public void testMoves(){
+        ChessModel m = new ChessModel();
+
+        // testing isValidMove and move for the pawn
+        assertTrue(m.isValidMove(new Move(6, 1, 4, 1)));
+        m.move(new Move(6, 1, 4, 1));
+        assertTrue(m.pieceAt(4, 1).type().equals("Pawn"));
+
+        // testing isValidMove and move for the bishop
+        assertTrue(m.isValidMove(new Move(7, 2, 6, 1)));
+        m.move(new Move(7, 2, 6, 1));
+        assertTrue(m.pieceaAt(6, 1).type().equals("Bishop"));
+
+        // testing isValidMove and move for the knight
+        assertTrue(m.isValidMove(new Move(7, 1, 5, 2)));
+        m.move(new Move(7, 1, 5, 2));
+        assertTrue(m.pieceAt(5, 2).type().equals("Knight"));
+
+        m.move(new Move(6, 0, 4, 0));
+
+        // testing isValid Move and move for the rook
+        assertTrue(m.isValidMove(new Move(7, 0, 5, 0)));
+        m.move(new Move(7, 0, 5, 0));
+        assertTrue(m.pieceAt(5, 0).type().equals("Rook"));
+
+        move(new Move(6, 4, 4, 4));
+
+        //testing isValidMove and move for the Queen
+        assertTrue(m.isValidMove(new Move(7, 3, 4, 6)));
+        m.move(new Move(7, 3, 4, 6));
+        assertTrue(m.pieceAt(4, 6).type().equals("Queen"));
+
+        //testing isValidMove and move for the King
+        assertTrue(m.isValidMove(new Move(7, 4, 7, 3)));
+        m.move(new Move(7, 4, 7, 3));
+        assertTrue(m.pieceAt(7, 4).type().equals("King"));
+
+    }
+
+    @Test
+    public void testCheck(){
+        // testing the inCheck method
+        ChessModel m = new ChessModel();
+        setPiece(7, 3, new Rook(Player.BLACK));
+        setPiece(7, 5, new Rook(Player.BLACK));
+        setPiece(6, 4, new Rook(Player.BLACK));
+        setPiece(6, 5, new Bishop(Player.BLACK));
+        setPiece(6, 3, new Bishop(Player.BLACK));
+
+        assertTrue(m.inCheck(Player.WHITE));
+
+    }
+
+    @Test
+    public void testUndo(){
+        //testing the undo method
+        ChessModel m = new ChessModel();
+        m.move(new Move(6, 1, 4, 1));
+        m.move(new Move(7, 2, 6, 1));
+        m.undo(1);
+        assertTrue(m.pieceAt(7, 2).type().equals("Bishop"));
+
+
+    }
+
+}
